2eb9bc602c6cef3fbdf40831ad65268ab4f2ff03
Karolis Tarasaitis <karolistarasaitis@gmail.com>
2016-10-13 05:54:26 +0300

    SpringBoot RootService

---
+++ rootService/src/main/java/shooter/root/controller/HelloController.java
@@ -0,0 +1,18 @@
+package shooter.root.controller;
+
+/**
+ * Created by owner on 10/13/16.
+ */
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
